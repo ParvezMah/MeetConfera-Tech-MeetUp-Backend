aedc8b8205bd78c33c510c05ad7b905fc57b24dd
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-12-07 10:44:37 +0600

    feat(user) : Added Searching and Sorting

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -37,9 +37,9 @@ const createAdmin = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
-    const {page, limit} = req.query;
+    const {page, limit, searchTerm, sortBy, sortOrder} = req.query;
 
-    const result = await UserService.getAllFromDB({page:Number(page), limit: Number(limit)});
+    const result = await UserService.getAllFromDB({page:Number(page), limit: Number(limit), searchTerm, sortBy, sortOrder});
 
     sendResponse(res, {
         statusCode: 200,
